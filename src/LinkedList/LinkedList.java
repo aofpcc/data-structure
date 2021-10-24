d685fcfbe38565f8b4e553215fbb51ab561047fc
Bunyarit Puangthamawathanakun <aofpc1@gmail.com>
2021-10-24 15:46:24 +0100

    Add linked list main & length, head

--- src/LinkedList/LinkedList.java
+++ src/LinkedList/LinkedList.java
@@ -1,6 +1,25 @@
 package LinkedList;
 
 public class LinkedList<T> {
+	public Node<T> head;
+	
+	private void checkIndexOutOfBounds(int index, boolean lessThanOnly) {
+		final int length = this.length();
+		if (index < 0 || (lessThanOnly && index >= length) || (!lessThanOnly && index > length) ) {
+			throw new IndexOutOfBoundsException();
+		}
+	}
+	
+	private int length() {
+		int count = 0;
+		Node<T> iterator = head;
+		while(iterator != null) {
+			iterator = iterator.next;
+			count++;
+		}
+		return count;
+	}
+	
 	// TODO: implement adding first
 	public boolean add(T value) {
 		throw new UnsupportedOperationException();
@@ -26,6 +45,19 @@ public class LinkedList<T> {
 	
 	// TODO: print from start to end with index horizontally
 	public void print () {
-		throw new UnsupportedOperationException();	
+		StringBuilder sbIndex = new StringBuilder();
+		sbIndex.append("Index  :\t");
+		StringBuilder sbValue = new StringBuilder();
+		sbValue.append("Values :\t");
+		int i = 0;
+		Node<T> iterator = head;
+		while(iterator != null) {
+			sbIndex.append(i +  "\t");
+			sbValue.append(iterator.value() + "\t");
+			i++;
+			iterator = iterator.next;
+		}
+		System.out.println(sbValue);
+		System.out.println(sbIndex);
 	}
 }
--- src/LinkedList/LinkedListMain.java
+++ src/LinkedList/LinkedListMain.java
@@ -2,6 +2,39 @@ package LinkedList;
 
 public class LinkedListMain {
 	public static void main(String ...args) {
-		System.out.println("Hello");
+		LinkedList<Integer> list = new LinkedList<Integer>();
+		System.out.println("Appending 1, 10, 5 accordingly");
+		list.add(1);
+		list.add(10);
+		list.add(5);
+		list.add(5);
+		list.add(6);
+		list.add(5);
+		list.print();
+		
+		System.out.println("\nAdd value at specific position");
+		list.add(2, 1);
+		list.add(4, 3);
+		list.print();
+		
+		System.out.println("\nGet value at specific position");
+		int d0 = list.get(0);
+		int d2 = list.get(2);
+		int d4 = list.get(4);
+		System.out.println("[0] = " + d0 + ", [2] = " + d2 + ", [4] = " + d4);
+		
+		System.out.println("\nRemove value at specific position");
+		System.out.println("Remove value at index 4");
+		list.removeAt(4);
+		System.out.println("Remove value at index 0");
+		list.removeAt(0);
+		list.print();
+		
+		System.out.println("\nRemove value from list");
+		System.out.println("Remove 5");
+		list.remove(5);
+		System.out.println("Remove 0");
+		list.remove(0);
+		list.print();
 	}
 }
